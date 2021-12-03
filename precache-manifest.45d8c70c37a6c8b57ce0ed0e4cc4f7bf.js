3befc87d1a4c90b501da5cd5fbb7d8692a667979
matthewquain <matthewquain@users.noreply.github.com>
2021-12-03 15:29:12 +0000

    deploy: 06c7fbc40c411cd203b0d80e3a188e553863d978

---
+++ precache-manifest.45d8c70c37a6c8b57ce0ed0e4cc4f7bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e56a70ac71abfc922d58fee12a79ebc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "070c125c68e7fb3b66ad",
+    "url": "/static/css/main.bdd92c28.chunk.css"
+  },
+  {
+    "revision": "53a2a12442f785b2f957",
+    "url": "/static/js/2.a5c2a9a4.chunk.js"
+  },
+  {
+    "revision": "804582f658d51d83a7fa8ebd45b1f897",
+    "url": "/static/js/2.a5c2a9a4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "070c125c68e7fb3b66ad",
+    "url": "/static/js/main.838677ce.chunk.js"
+  },
+  {
+    "revision": "78cc47f66930bca56c35",
+    "url": "/static/js/runtime-main.21a3aa16.js"
+  }
+]);
